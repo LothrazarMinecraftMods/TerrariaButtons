9cb47c8e35fcc7737a033e5c9d91b0a2aaeb96ae
sam bassett <samson.bassett@gmail.com>
2015-11-23 21:28:01 -0800

    base button grid

--- src/main/java/com/lothrazar/terrariabuttons/client/GuiButtonSlotSingle.java
+++ src/main/java/com/lothrazar/terrariabuttons/client/GuiButtonSlotSingle.java
@@ -1,6 +1,9 @@
 package com.lothrazar.terrariabuttons.client;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class GuiButtonSlotSingle extends GuiButton
 { 
@@ -10,4 +13,27 @@ public class GuiButtonSlotSingle extends GuiButton
 		this.width=20;
 		this.height=20; 
 	} 
+	
+	@SideOnly(Side.CLIENT)
+    @Override
+    public boolean mousePressed(Minecraft mc, int mouseX, int mouseY)
+    {
+    	boolean pressed = super.mousePressed(mc, mouseX, mouseY);
+    	
+    	if(pressed)
+    	{ 
+    		System.out.printf("\n",this.id,row,col);
+    		//ModTerrariaButtons.instance.network.sendToServer(new LootAllPacket(new NBTTagCompound()));
+    	}
+    	
+    	return pressed;
+    }
+
+	private int row;
+	private int col;
+	public void setLoc(int r, int c)
+	{
+		row=r;
+		col=c;
+	}
 }
--- src/main/java/com/lothrazar/terrariabuttons/client/GuiScreenLocks.java
+++ src/main/java/com/lothrazar/terrariabuttons/client/GuiScreenLocks.java
@@ -16,16 +16,20 @@ public class GuiScreenLocks extends GuiScreen
 	public void initGui()
 	{
 		int buttonId = 0;
-		int x = Minecraft.getMinecraft().displayWidth/2;
-		int y = Minecraft.getMinecraft().displayHeight/2;
-		int padding = 8;
+		int x = Minecraft.getMinecraft().displayWidth/4;
+		int y;// = Minecraft.getMinecraft().displayHeight/2;
+		int w = 20; // button width is fixed
+		int padding = 8;//space between
+		GuiButtonSlotSingle b;
 		for(int rows = 0; rows < 3; rows++){
+			y = Minecraft.getMinecraft().displayHeight/2 + rows*20 + padding;
 			for(int cols = 0; cols < 9; cols++){
 
 				buttonId = rows + cols;
-				x = cols * 20 + padding;
-				y = rows * 20 + padding;
-				buttonList.add(new GuiButtonSlotSingle(buttonId, x,y));
+				x += w + padding;
+				b = new GuiButtonSlotSingle(buttonId, x,y);
+				b.setLoc(rows,cols);
+				buttonList.add(b);
 			}
 		}
 	}
